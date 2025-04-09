39b0086627e921c3e1d50c5171306b8f311ec53f
Wouter Born <github@maindrain.net>
2025-04-09 09:27:02 +0200

    Apply Spotless

--- setup/src/main/java/org/openremote/manager/setup/custom/CustomSetupTasks.java
+++ setup/src/main/java/org/openremote/manager/setup/custom/CustomSetupTasks.java
@@ -1,9 +1,6 @@
 /*
  * Copyright 2021, OpenRemote Inc.
  *
- * See the CONTRIBUTORS.txt file in the distribution for a
- * full listing of individual contributors.
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -15,30 +12,30 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 package org.openremote.manager.setup.custom;
+
+import java.util.Arrays;
+import java.util.List;
 
 import org.openremote.model.Container;
 import org.openremote.model.setup.Setup;
 import org.openremote.model.setup.SetupTasks;
 
-import java.util.Arrays;
-import java.util.List;
-
 public class CustomSetupTasks implements SetupTasks {
 
-    public static final String PRODUCTION = "production";
+  public static final String PRODUCTION = "production";
 
-    @Override
-    public List<Setup> createTasks(Container container, String setupType, boolean keycloakEnabled) {
+  @Override
+  public List<Setup> createTasks(Container container, String setupType, boolean keycloakEnabled) {
 
-        boolean isProduction = PRODUCTION.equalsIgnoreCase(setupType);
+    boolean isProduction = PRODUCTION.equalsIgnoreCase(setupType);
 
-        // Add custom Setup task implementations here with tasks optionally dependent on setupType
-        return Arrays.asList(
-            new CustomKeycloakSetup(container, isProduction),
-            new CustomManagerSetup(container)
-        );
-    }
+    // Add custom Setup task implementations here with tasks optionally dependent on setupType
+    return Arrays.asList(
+        new CustomKeycloakSetup(container, isProduction), new CustomManagerSetup(container));
+  }
 }
