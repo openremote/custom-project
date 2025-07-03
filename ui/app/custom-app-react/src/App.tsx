f10b31ea287bed1bf872cd7c39774913dbcc415d
MartinaeyNL <business@martinaey.nl>
2025-07-03 12:02:17 +0200

    Added example for listening to events

--- ui/app/custom-app-react/src/App.tsx
+++ ui/app/custom-app-react/src/App.tsx
@@ -1,18 +1,31 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import reactLogo from "./assets/react.svg";
 import {InputType, OrMwcInput} from "@openremote/or-mwc-components/or-mwc-input";
 import "./App.css";
 
+/**
+ * In React, for web components to be recognized, it's common to add each HTML tag in the JSX IntrinsicElements interface.
+ * Be aware; your IDE might still not understand these web components correctly.
+ */
 declare global {
 	namespace JSX {
 		interface IntrinsicElements {
-			'or-mwc-input': OrMwcInput
+			"or-mwc-input": OrMwcInput
 		}
 	}
 }
 
 function App() {
 	const [count, setCount] = useState(0);
+	const buttonRef = useRef(null);
+	const handleButtonClick = () => setCount(count => count + 1);
+
+	useEffect(() => {
+		(buttonRef.current as any)?.addEventListener("or-mwc-input-changed", handleButtonClick);
+		return () => {
+			(buttonRef.current as any)?.removeEventListener("or-mwc-input-changed", handleButtonClick);
+		};
+	}, []);
 
 	return (
 		<div className="App">
@@ -29,11 +42,11 @@ function App() {
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
+				<or-mwc-input ref={buttonRef} type={InputType.BUTTON} outlined label="An OpenRemote button"></or-mwc-input>
 			</div>
 			<p className="read-the-docs">
-				Click on the Rspack and React logos to learn more
+				Click on the React logo to learn more
 			</p>
-			<or-mwc-input type={InputType.BUTTON} label="This is a button"></or-mwc-input>
 		</div>
 	);
 }
