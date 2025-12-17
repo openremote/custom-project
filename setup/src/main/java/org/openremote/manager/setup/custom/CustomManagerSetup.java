cd3a23a25e8e9746c153297b1ceb90d0e909007a
Wouter Born <github@maindrain.net>
2025-12-17 17:13:00 +0100

    Apply Spotless

--- setup/src/main/java/org/openremote/manager/setup/custom/CustomManagerSetup.java
+++ setup/src/main/java/org/openremote/manager/setup/custom/CustomManagerSetup.java
@@ -1,9 +1,6 @@
 /*
  * Copyright 2021, OpenRemote Inc.
  *
- * See the CONTRIBUTORS.txt file in the distribution for a
- * full listing of individual contributors.
- *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
@@ -15,7 +12,9 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 package org.openremote.manager.setup.custom;
 
@@ -26,15 +25,15 @@ import org.openremote.model.asset.impl.ThingAsset;
 
 public class CustomManagerSetup extends ManagerSetup {
 
-    public CustomManagerSetup(Container container) {
-        super(container);
-    }
+  public CustomManagerSetup(Container container) {
+    super(container);
+  }
 
-    @Override
-    public void onStart() throws Exception {
-        super.onStart();
+  @Override
+  public void onStart() throws Exception {
+    super.onStart();
 
-        ThingAsset thing = new ThingAsset("Custom Thing").setRealm(Constants.MASTER_REALM);
-        thing = assetStorageService.merge(thing);
-    }
+    ThingAsset thing = new ThingAsset("Custom Thing").setRealm(Constants.MASTER_REALM);
+    thing = assetStorageService.merge(thing);
+  }
 }
